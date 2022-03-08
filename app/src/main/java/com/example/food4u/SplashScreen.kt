dcc871379a7e8599fef8bc20dd7a0b5141af7d84
kai-T3 <tankl-wm19@student.tarc.edu.my>
2022-03-08 23:18:01 +0800

    splash screen

---
+++ app/src/main/java/com/example/food4u/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.food4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
