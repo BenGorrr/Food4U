5928e9e2daead65998fd82688a9017b713f9549b
kai-T3 <tankl-wm19@student.tarc.edu.my>
2022-04-16 17:38:46 +0800

    Before Changing From Profile act to fragment

---
+++ app/src/main/java/com/example/food4u/fragments/EditProfileFragment.kt
@@ -0,0 +1,178 @@
+package com.example.food4u.fragments
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.food4u.ChangePasswordActivity
+import com.example.food4u.databinding.FragmentEditProfileBinding
+import com.example.food4u.modal.User
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
+import com.example.food4u.ProfilePage
+import com.example.food4u.userSignin
+import kotlinx.android.synthetic.main.activity_profile_page.*
+
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
+import java.util.*
+
+
+class EditProfileFragment : Fragment() {
+    private lateinit var binding: FragmentEditProfileBinding
+    private lateinit var database: DatabaseReference
+    private var selectedUri: Uri? = null
+    private lateinit var ref:StorageReference
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        ref = FirebaseStorage.getInstance().reference
+        selectedUri = Uri.parse("")
+
+        val editName = binding.tfEditName
+        val editBd = binding.tfEditBirthDate
+        val editAddress = binding.tfEditAddress
+        val editPhoneNo = binding.tfPhoneNo
+        val btnChangePw = binding.btnChangePw
+        val btnSave = binding.btnSave
+        val editProfPicIcon = activity!!.profPic
+
+        val DB = Firebase.database("https://food4u-9d1c8-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        database= DB.getReference("userDB")
+        database.child("User").child(Firebase.auth.uid.toString()).get()
+            .addOnSuccessListener { rec->
+                if(rec!=null){
+                    editName.setHint(rec.child("name").value.toString())
+                    editBd.setHint(rec.child("birthDate").value.toString())
+                    editAddress.setHint(rec.child("address").value.toString())
+                    editPhoneNo.setHint(rec.child("phoneNo").value.toString())
+                }
+            }
+
+
+
+        editProfPicIcon.setOnClickListener {
+            Log.i("check","yesla")
+            startForResult.launch("image/*")
+        }
+
+        btnSave.setOnClickListener{
+            val dateFromEditText = editBd.text.toString()
+            if(dateFromEditText!="" && dateFromEditText.length!=10 ) {
+                binding.tfEditBirthDate.setError("Your date is incorrect!")
+            }
+            else{
+                activity!!.tvMyProfile.visibility = View.VISIBLE
+                activity!!.tvEditProfile.visibility = View.INVISIBLE
+                activity!!.tvUserDisplayName.visibility = View.VISIBLE
+                activity!!.tvNumOfDonation.visibility = View.VISIBLE
+                activity!!.btnManagePayment.visibility = View.VISIBLE
+                activity!!.editIcon.visibility = View.VISIBLE
+                database.child("User").child(Firebase.auth.uid.toString()).get()
+                    .addOnSuccessListener { rec->
+                        if(rec!=null){
+                            if(editName.text.toString()!=""){
+                                database.child("User").child(Firebase.auth.uid.toString()).child("name").setValue(editName.text.toString())
+                            }else database.child("User").child(Firebase.auth.uid.toString()).child("name").setValue(rec.child("name").value.toString())
+                            if(dateFromEditText!=""&& dateFromEditText.length==10){
+                                database.child("User").child(Firebase.auth.uid.toString()).child("birthDate").setValue(dateFromEditText)
+                            }
+                            if(editAddress.text.toString()!=""){ //if there is edited entry
+                                database.child("User").child(Firebase.auth.uid.toString()).child("address").setValue(editAddress.text.toString())
+                            }else database.child("User").child(Firebase.auth.uid.toString()).child("address").setValue(rec.child("address").value.toString()) //remain same value
+                            if(editPhoneNo.text.toString()!=""){ //if there is edited entry
+                                if((editPhoneNo.length() in 10..11)) database.child("User").child(Firebase.auth.uid.toString()).child("phoneNo").setValue(editPhoneNo.text.toString())
+                                else binding.tfPhoneNo.setError("Your phone number is invalid!")
+                            }else database.child("User").child(Firebase.auth.uid.toString()).child("phoneNo").setValue(rec.child("phoneNo").value.toString()) //remain same value
+                        }
+                    }
+                val selectedUri = selectedUri
+                if(selectedUri!=null) uploadToFirebase(selectedUri)
+                //replaceFragment(ProfileFragment())
+                val intent = Intent(activity, ProfilePage::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                activity!!.finish()
+            }
+
+//            activity!!.supportFragmentManager.beginTransaction().detach(ProfileFragment()).attach(ProfileFragment()).commit()
+//            replaceFragment(ProfileFragment())
+        }
+
+        btnChangePw.setOnClickListener{
+            val intent = Intent(activity, ChangePasswordActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            activity!!.finish()
+        }
+        return binding.root
+    }
+
+    val startForResult = registerForActivityResult(
+        ActivityResultContracts.GetContent()){ uri->
+            if(uri!=null){
+                selectedUri = uri
+                activity!!.profPic.setImageURI(selectedUri)
+            }
+    }
+
+    fun uploadToFirebase(fileUri:Uri){
+        if(fileUri.toString().isNotEmpty()){
+            val filename = UUID.randomUUID().toString() + ".png"
+            val imgRef = ref.child("images/user/" + filename)
+            imgRef.putFile(fileUri)
+                .addOnSuccessListener (
+                    OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
+                        taskSnapshot.storage.downloadUrl.addOnSuccessListener {
+                            val imageUrl = it.toString()
+                            database.child("User").child(Firebase.auth.uid.toString()).child("imgUrl").setValue(imageUrl)
+                        }
+                    })
+                ?.addOnFailureListener(OnFailureListener { e ->
+                    print(e.message)
+                })
+        }
+
+    }
+
+
+    //change fragment
+//    private fun replaceFragment(fragment1: Fragment) {
+//        val fragmentManager = activity!!.supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.container, fragment1)
+//        fragmentTransaction.commit()
+//    }
+
+    //    public fun passValues(): User {
+//        val editName = binding.tfEditName
+//        val editBd = binding.tfEditBirthDate
+//        val editAddress = binding.tfEditAddress
+//        val editPhoneNo = binding.tfPhoneNo
+//        val dateFromEditText = editBd.text.toString()
+//        val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy")
+//        val dateTime = simpleDateFormat.parse(dateFromEditText)
+//        newUser = User("", editName.text.toString(), dateTime, editAddress.text.toString(), editPhoneNo.text.toString(), 0)
+//        return newUser
+//    }
+}
+
